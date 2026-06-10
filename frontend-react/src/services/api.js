6c8ba98dbcc896c6f671f426311d5906542f639a
unknown <your@email.com>
2026-06-10 20:28:38 +0530

    fix: api.js base url

--- frontend-react/src/services/api.js
+++ frontend-react/src/services/api.js
@@ -1,5 +1,4 @@
 export const API_URL =
 
-  import.meta.env.VITE_API_URL ||
+  import.meta.env.VITE_API_URL || "import.meta.env.VITE_API_URL/api" || "http://localhost:5000";
 
-  "import.meta.env.VITE_API_URI/api";
